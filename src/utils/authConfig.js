57b0b8600bee3af69c0aa064cfe80a8cfeddb74d
Daniel Méndez <desmendezd@gmail.com>
2025-04-13 21:24:12 -0500

    Se agrega un nuevo registro de la aplicación, debido a que microsoft eliminó la app, así que se usa un nuevo tenat y cliente id

--- src/utils/authConfig.js
+++ src/utils/authConfig.js
@@ -1,6 +1,6 @@
 export const msalConfig = {
     auth: {
-      clientId: "e3fb8881-8c82-4345-8048-68db1ca177f5",
+      clientId: "6d916e6b-a36a-48bd-a314-aea85abbf595",
       authority: "https://login.microsoftonline.com/87543dca-0f58-48e4-af72-6410eb960e17",
       redirectUri: "http://localhost:5173", // << Asegúrate que esto esté así
     },
@@ -12,5 +12,6 @@ export const msalConfig = {
   
   export const loginRequest = {
     scopes: ["User.Read"],
+    prompt: "select_account", // fuerza que siempre pida escoger cuenta
   };
   
